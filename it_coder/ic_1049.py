a39b080fde19e16cbbfb7a5847651e7fcc09a92d
Le Nhat Tung <49211930+lenhattung@users.noreply.github.com>
2025-11-17 15:13:43 +0700

    Delete it_coder directory

--- it_coder/ic_1049.py
+++
@@ -1,6 +0,0 @@
-n, q = map(int, input().split())
-result = [0] * n
-layers = [0]
-for _ in range(q):
-    r, p = map(int, input())
-    
--- it_coder/ic_1134.py
+++
@@ -1,4 +0,0 @@
-def is_Prime(n):
-    arr = [i for i in range(2, )]
-
-n = int(input())
